f06ebfba2f5c58df47ec0321f4baf28541c664f8
Hans Fangohr <hans.fangohr@mpsd.mpg.de>
2021-09-09 16:07:38 +0200

    Adding one function and one test

---
+++ example.py
@@ -0,0 +1,12 @@
+def f(n):                     # "production code"
+    s = 0
+    # Loop from 0 to n:
+    for i in range(1, n + 1):
+        s = s + i
+    return s
+
+
+def test_f():                 # "test code"
+    assert f(3) == 0 + 1 + 2 + 3
+    assert f(5) == 15
+    assert f(10) == 55
